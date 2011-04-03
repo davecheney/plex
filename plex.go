e41c9531f1176a52e771350fd07c19e6a59de924
Dave Cheney <dave@cheney.net>
2011-04-03 12:13:28 +1000

    Plex test

--- plex.go
+++ plex.go
@@ -1,2 +1,21 @@
 package plex
 
+import (
+	"net"
+)
+
+type Plex struct {
+	conn net.Conn
+	// unexported
+}
+
+func runner(p *Plex) {
+
+}
+
+
+func NewPlex(conn net.Conn) *Plex {
+	p := &Plex{conn}
+	go runner(p)
+	return p
+}
--- plex_test.go
+++ plex_test.go
@@ -1,5 +1,11 @@
 package plex
 
-import "testing"
+import (
+	"testing"
+	"net"
+)
 
-
+func TestPipePlex(t *testing.T) {
+	c1, c2 := net.Pipe()
+ 	_, _ = NewPlex(c1), NewPlex(c2)
+}
